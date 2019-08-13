114e3b261b02390b64886994c4b9c7f580c1d5e7
Jarblack <jharblack09@gmail.com>
2019-08-13 14:36:55 -0500

    pantalla CapturaDatos1

--- RondasEcopetrol/Base/ViewModelLocator.cs
+++ RondasEcopetrol/Base/ViewModelLocator.cs
@@ -14,11 +14,14 @@
             _container.RegisterType<BajarRondaViewModel>();
             _container.RegisterType<IniciarSesionViewModel>();
             _container.RegisterType<HacerRondaViewModel>();
+            _container.RegisterType<CapturaDatos1ViewModel>();
         }
         public MainPageViewModel MainPageViewModel => _container.Resolve<MainPageViewModel>();
         public BajarRondaViewModel BajarRondaViewModel => _container.Resolve<BajarRondaViewModel>();
         public IniciarSesionViewModel IniciarSesionViewModel => _container.Resolve<IniciarSesionViewModel>();
 
         public HacerRondaViewModel HacerRondaViewModel => _container.Resolve<HacerRondaViewModel>();
+
+        public CapturaDatos1ViewModel CapturaDatos1ViewModel => _container.Resolve<CapturaDatos1ViewModel>();
     }
 }
---
+++ RondasEcopetrol/ViewModels/CapturaDatos1ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RondasEcopetrol.Base;
+using Windows.UI.Xaml.Navigation;
+
+namespace RondasEcopetrol.ViewModels
+{
+    public class CapturaDatos1ViewModel : ViewModelBase
+    {
+        public override Task OnNavigatedFrom(NavigationEventArgs args)
+        {
+            //throw new NotImplementedException();
+            return null;
+        }
+
+        public override Task OnNavigatedTo(NavigationEventArgs args)
+        {
+            //throw new NotImplementedException();
+            return null;
+        }
+    }
+}
