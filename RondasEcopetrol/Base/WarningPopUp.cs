5b4eb3c17f4203026cb8c4485785fd3d997b4b2d
mamejiar <mejia.martin@gmail.com>
2019-08-22 18:52:45 -0500

    Ajustes en captura Datos 1

---
+++ RondasEcopetrol/Base/WarningPopUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace RondasEcopetrol.Base
+{
+    public abstract class WarningPopUp
+    {
+        private bool _yes;
+        protected bool navFromSheet;
+
+        public abstract string getTitle();
+        public abstract string getDescription();
+        public abstract void noClick();
+
+        public abstract void yesClick();
+
+        public async Task<bool> showAsync()
+        {
+            var messageDialog = new MessageDialog(getDescription());
+            messageDialog.Title = getTitle();
+            messageDialog.Commands.Add(new UICommand(
+                "Sí", new UICommandInvokedHandler(yesCommand)));
+            messageDialog.Commands.Add(new UICommand(
+                "No", new UICommandInvokedHandler(noCommand)));
+            await messageDialog.ShowAsync();
+            return _yes;
+        }
+
+        private void noCommand(IUICommand command)
+        {
+            _yes = false;
+            noClick();
+        }
+
+        private void yesCommand(IUICommand command)
+        {
+            _yes = true;
+            yesClick();
+        }
+    }
+}
---
+++ RondasEcopetrol/PopUps/RondasFinalizarPopUp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using RondasEcopetrol.Base;
+using RondasEcopetrol.Models;
+using RondasEcopetrol.Views;
+using RondasEcopetrol.ServerUtils;
+
+namespace RondasEcopetrol.PopUps
+{
+    public class RondasFinalizarPopUp : WarningPopUp
+    {
+        private Frame _curFrame;
+
+        public RondasFinalizarPopUp(Frame curFrame, bool navFromSheet)
+        {
+            _curFrame = curFrame;
+            this.navFromSheet = navFromSheet;
+        }
+
+        public override string getDescription()
+        {
+            return "No existen más datos por capturar.\nDesea terminar la ronda en este momento?";
+        }
+
+        public override string getTitle()
+        {
+            return "Finalizar Ronda";
+        }
+
+        public override void noClick()
+        {
+            if (navFromSheet)
+            {
+                //TODO
+                /*Sheet.NEXT_TRIGGER = false;*/
+                RondasLector.CurrentRonda.getLastWork();
+                _curFrame.Navigate(typeof(CapturaDatos2));
+            }
+            else
+            {
+                _curFrame.Navigate(typeof(CapturaDatos1));
+            }
+        }
+
+        public override void yesClick()
+        {
+            Rondas rondas1 = RondasLector.CurrentRonda;
+            rondas1.ensureProccessAll();
+            String rondaXML = rondas1.getStringSave();
+            FileUtils.writeXmlData("rnd" + rondas1.MessageID + "Save.xml", rondaXML);
+            //System.IO.FileStream stream = new System.IO.FileStream("/My Documents/ronda" + rondas1.Id + DateTime.Now.ToString("yyyyMMddHHmmss"), System.IO.FileMode.Create);
+            //byte[] b = System.Text.UTF8Encoding.UTF8.GetBytes(rondaXML);
+            //stream.Write(b, 0, b.Length);
+            //stream.Close();
+
+            //TODO Pendiente traducir esto
+            /*DataRow[] rowArray1 = RondasApp.app.DsetRondasDown.Tables["Ronda"].Select("Message_ID=" + rondas1.MessageID);
+            rowArray1[0]["Complete"] = "true";
+            rowArray1[0].AcceptChanges();
+            rowArray1[0].Table.AcceptChanges();
+            if (rondas1.Suspend)
+            {
+                RondasApp.deleteSuspend(rondas1);
+            }*/
+        }
+    }
+}
